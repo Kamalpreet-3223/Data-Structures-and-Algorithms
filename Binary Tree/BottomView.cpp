5c02b1b8ce9f23f7142162136be828e1124dd560
KAMALPREET SINGH <73851933+Kamalpreet-3223@users.noreply.github.com>
2023-06-23 04:00:12 +0530

    DAY23

---
+++ Binary Tree/BottomView.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h> 
+/*************************************************************
+ 
+    Following is the Binary Tree node structure.
+
+    class BinaryTreeNode 
+    {
+    public : 
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+*************************************************************/
+
+vector<int> bottomView(BinaryTreeNode<int> * root){
+
+    vector<int> ans;
+    map<int,int> mpp;
+    queue<pair<BinaryTreeNode<int>*,int>> q;
+    q.push({root,0});
+    while(!q.empty()){
+        auto it = q.front();
+        q.pop();
+        BinaryTreeNode<int>* node=it.first;
+        int line=it.second;
+        mpp[line]=node->data;
+        if(node->left!=NULL){
+            q.push({node->left,line-1});
+        }
+        if(node->right!=NULL){
+            q.push({node->right,line+1});
+        }
+    }
+    
+    for(auto it : mpp){
+        ans.push_back(it.second);
+    }
+    return ans;
+    
+    
+}
